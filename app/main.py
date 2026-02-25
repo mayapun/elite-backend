2b2151dcef3936f4810c80f97931ed2ce5cd91dd
Maya <maya@Mayas-Air.lan>
2026-02-25 11:54:37 -0500

    Change origins to test local to deployed backend

--- app/main.py
+++ app/main.py
@@ -15,12 +15,16 @@ from app.core.config import settings
 
 app = FastAPI()
 
-if settings.ENVIRONMENT == "development":
-    origins = ["http://localhost:3000",
-    "http://127.0.0.1:3000"
-    ]
-else:
-    origins = ["https://vercel.com"]
+# if settings.ENVIRONMENT == "development":
+#     origins = ["http://localhost:3000",
+#     "http://127.0.0.1:3000"
+#     ]
+# else:
+#     origins = ["https://vercel.com"]
+
+origins = ["http://localhost:3000",
+"http://127.0.0.1:3000",
+"https://vercel.com"]
 
 app.add_middleware(
     CORSMiddleware,
